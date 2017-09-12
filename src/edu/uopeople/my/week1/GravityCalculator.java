6d3e6e1466b0e373585943d50244edf69043baa0
Cavalieredraghi <Biggestantman@gmail.com>
2017-09-12 19:55:07 -0400

    Adding week 1 programming exercise

--- src/edu/uopeople/my/week1/GravityCalculator.java
+++ src/edu/uopeople/my/week1/GravityCalculator.java
@@ -1,5 +1,29 @@
 package edu.uopeople.my.week1;
 
-public class GravityCalculator {
+import java.io.*;
 
+public class GravityCalculator {
+	public static void main(String[] arguments) {
+
+		double gravity = -9.81; // Earth's gravity in m/s^2
+
+		double initialVelocity = 0.0; // starting velocity of the object
+
+		double fallingTime = 10.0; // time in seconds that the object falls
+
+		double initialPosition = 1000.0; // Starting position in meters, the calculation will determine the ending
+											// position in meters
+
+	//	double finalPosition = 0.0;  Commenting out the initial Final Position
+		//Associating the variables above to of the assignment
+		double a = gravity;
+		double t = fallingTime;
+		double vi = initialVelocity;
+		double xi = initialPosition;
+		double xt = .5*a*(t*t)+vi*t+xi;
+		
+		System.out.println(
+				"The object's position after " + fallingTime + " seconds is 			" + xt  + " m."); //removed the Final Position from the println
+
+	}
 }
