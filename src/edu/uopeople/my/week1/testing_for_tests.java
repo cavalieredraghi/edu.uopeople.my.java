6b711fddc1b2d491127bb7a310864737c91a2db0
Cavalieredraghi <Biggestantman@gmail.com>
2017-10-10 21:49:17 -0400

    merging an old commit

--- src/edu/uopeople/my/week1/testing_for_tests.java
+++ src/edu/uopeople/my/week1/testing_for_tests.java
@@ -3,17 +3,16 @@ package edu.uopeople.my.week1;
 import java.io.*;
 
 public class testing_for_tests {
-
-	public static void main(String[] args) {
-		int x;
-		x = 5;
-		switch (x) {
-		case 1:
-			break;
-		case 2:
-			break;
-		default:
-			System.out.println("The choice was not found");
+	public static void main(String[] arguments) {
+		String x = stringFunction("mississippi");
+		String stringFunction(String str){
+			String test;
+			int i;
+			test = "";
+			for (i = str.length() - 1; i >= 0; i--) {
+				test = test + str.charAt(i);
+			}
+			return test;
 		}
 	}
 }
